1ce96815d79c4aa29e077e8a5e7eda21118b635a
Shivelle <shivelle@gmx.net>
2017-07-18 19:51:55 +0200

    fix scroll bar

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,15 @@
     const scrollLine = document.querySelector('.scroll-line');
     const mainContent = document.querySelector('.main-content'); 
 
+
     // functions
     function fillScrollLine() {
+      scrollLine.style.display = "block"; 
       const windowHeight    = window.innerHeight;
       // const fullHeight      = document.body.clientHeight;
       const fullHeight 		= mainContent.clientHeight; 
       const scrolled        = window.scrollY;
-      const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
+      const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 50;
       scrollLine.style.width = `${percentScrolled}%`;
     }
 
