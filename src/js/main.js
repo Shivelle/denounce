f513c300fc7e4ef4e4bde4096db21c8c7bf939fc
Shivelle <shivelle@gmx.net>
2017-07-24 19:15:45 +0200

    add placeholder content

--- src/js/main.js
+++ src/js/main.js
@@ -258,3 +258,28 @@ $berry: #93032E;
 
     // some hello msg
 	console.log("woof!");
+
+
+
+	// convert this: ACCORDION
+	const accordion = document.querySelector('.accordion'); 
+const items 	= accordion.querySelectorAll('li'); 
+const questions = accordion.querySelectorAll('.question'); 
+
+// functions
+function toggleAccordion() {
+	const thisItem = this.parentNode;
+
+	items.forEach(item => {
+		if(thisItem == item) {
+			thisItem.classList.toggle('open'); 
+			return
+		}
+			item.classList.remove('open'); 
+	});  
+
+}
+
+
+// event listeners
+questions.forEach(question => question.addEventListener('click', toggleAccordion));
